555b4eb9b4dc8f44cc7d28f6d359f522155b4652
aleks9045 <aleksey9045@gmail.com>
2025-03-25 20:47:55 +0300

    add users mapper test

--- src/test/java/org/example/bvito/mappers/UsersMapperTests.java
+++ src/test/java/org/example/bvito/mappers/UsersMapperTests.java
@@ -12,5 +12,16 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 
 public class UsersMapperTests {
+    private final UsersMapper usersMapper = UsersMapper.INSTANCE;
 
+    @Test
+    void toEntityTest() {
+        Users users = Users.newInstance();
+
+        UsersSchema usersSchema = UsersSchema.newInstance();
+
+        Users usersFromSchema = usersMapper.toEntity(usersSchema);
+        usersFromSchema.setU_id(1);
+        assertEquals(users, usersFromSchema);
+    }
 }
